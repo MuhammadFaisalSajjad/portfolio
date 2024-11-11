38b34704c0cb2efe319dd403cfb1ffe35e9c612b
M Faisal Sajjad <f.sajjad530@gmail.com>
2024-11-11 11:41:00 +0500

    cors

--- server/server.js
+++ server/server.js
@@ -37,7 +37,9 @@ const corsOptionsDelegate = function (req, callback) {
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptionsDelegate));
+// app.use(cors(corsOptionsDelegate));
+app.use(cors({ origin: '*' }));
+
 
 // DB Connection
 dbConnection(MONGODB_URL);
